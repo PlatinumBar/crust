2b03ae8528cb7c9aff17ce951d0eec6b3ea33e1e
PlatinumBar <145838777+PlatinumBar@users.noreply.github.com>
2026-05-24 11:20:55 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use std::io;
+
+use beryllium::*;
+use beryllium::init::InitFlags;
+use beryllium::events::Event;
+use beryllium::video::CreateWinArgs;
+use beryllium::video::RendererFlags;
+
+const WIDTH: i32 = 1600;
+const HEIGHT: i32 = 1200;
+
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32
+}
+
+fn main() {
+
+    let mut counter = 0;
+
+    let sdl = Sdl::init(InitFlags::EVERYTHING);
+
+    let window = sdl
+        .create_renderer_window(CreateWinArgs { title: "COLLATZ", width: WIDTH, height: HEIGHT, ..Default::default()},
+                                                RendererFlags::ACCELERATED)
+        .unwrap();
+
+    let mut points = Vec::new();
+      'main_loop: loop {
+        while let Some((event, _timestamp)) = sdl.poll_events() {
+            match event {
+                Event::Quit => break 'main_loop,
+                _ => (),
+            }
+        }
+        counter += 1;
+        points.push(Point {x: counter, y: collatz(counter)});
+        println!("Info:: added new point: {:?}", points[points.len()-1]);
+    
+        window.set_draw_color(255, 255, 255, 254).unwrap();
+        window.clear().unwrap();
+
+        window.set_draw_color(0, 0, 0, 255).unwrap();
+        window.draw_points(&[[points[points.len()-1].x, points[points.len()-1].y - HEIGHT]]).unwrap();
+
+        window.present();
+    }
+
+}
+
+ 
+fn collatz(n: i32) -> i32{
+    let mut g:u128 = n.try_into().unwrap();
+    let mut i = 0;
+    loop {
+        if g == 1 {
+            return i;
+        } else if g % 2 == 0 {
+            g = g/2;
+            i+=1;
+        } else {
+            g = 3*g + 1;
+            i+=1;
+        }
+    }
+
+} 
